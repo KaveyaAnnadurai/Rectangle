93ea0e5cc5a9f50b255ceefe56a37d69b31754c6
KaveyaAnnadurai <kaveya.a@m2pfintech.com>
2023-03-29 15:39:11 +0530

    Initial commit done.

---
+++ src/test/java/com/m2p/AreaTests.java
@@ -0,0 +1,11 @@
+package com.m2p;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AreaTests {
+    Geometry geometry = new Geometry();
+
+
+}
